ce56fbcb5da2325798c291bbc15ff36b5aa7a6a6
uddeshrothe <uddesh.rothe1@gmail.com>
2024-07-30 22:01:29 +0530

    "Added auth route and crypto-js dependency for user registration with password encryption"

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const userRoute = require('./routes/user.router.js')
+const authRoute = require('./routes/auth.route.js')
 const dotenv = require('dotenv')
 dotenv.config()
 
@@ -10,6 +11,7 @@ app.use(express.json())
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("DB Connection is Successful")).catch((err) => console.log(err))
 
+app.use('/api/auth', authRoute)
 app.use('/api/user', userRoute)
 
 app.listen(process.env.PORT || 4001, () => {
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,4 +1,26 @@
 const router = require('express').Router()
+const User = require('../models/User.model')
+const CryptoJs = require('crypto-js')
+const dotenv = require('dotenv')
+dotenv.config()
 
+//Register
+router.post("/register", async (req, res) => {
+
+    const newUSer = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+    })
+
+    try {
+        const savedUser = await newUSer.save()
+        res.status(200).json(savedUser)
+        console.log(savedUser)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+})
 
 module.exports = router
