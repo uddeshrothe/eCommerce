29b7c87293c251bffcacbe3b0ffb3809a0d5d2ef
uddeshrothe <uddesh.rothe1@gmail.com>
2024-07-30 21:42:00 +0530

    "Added Express.js server with Mongoose connection and user route, updated package.json with dependencies and scripts"

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const userRoute = require('./routes/user.router.js')
+const dotenv = require('dotenv')
+dotenv.config()
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection is Successful")).catch((err) => console.log(err))
+
+app.use('/api/user', userRoute)
+
+app.listen(process.env.PORT || 4001, () => {
+    console.log("Server is listening!")
+})
