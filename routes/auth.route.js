f9b99658e9237b510b666ad8aa9f19fdca6f9968
uddeshrothe <uddesh.rothe1@gmail.com>
2024-07-31 23:13:34 +0530

    "Added login endpoint to auth route"

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -23,4 +23,23 @@ router.post("/register", async (req, res) => {
     }
 })
 
+//Login
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username })
+        if (!user)
+            return res.status(401).json("Wrong credentials!")
+
+        const hashedPassword = CryptoJs.AES.decrypt(user.password, process.env.PASS_SEC)
+        const password = hashedPassword.toString(CryptoJs.enc.Utf8)
+
+        if (password !== req.body.password)
+            return res.status(401).json("Wrong credentials!")
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router
